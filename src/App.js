35178d8c1be999121d003dc41d7c1949fc7d22a7
Hojung Lee <aychjay.l@gmail.com>
2019-10-19 13:27:48 -0700

    added props and set state

--- src/App.js
+++ src/App.js
@@ -4,12 +4,46 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Weather from "./component/weather.component";
 
-function App() {
-  return (
-    <div className="App">
-      <Weather/>
-    </div>
-  );
+const API_key = "7da9cc8ca53a06d6e5d867f3451b13d9";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      city: undefined,
+
+
+    };
+    this.getWeather();
+
+  }
+  getWeather = async()=>{
+    const api_call = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=7da9cc8ca53a06d6e5d867f3451b13d9');
+    const response = await api_call.json();
+    console.log(response);
+
+    this.setState({
+      city: response.name,
+      country: response.sys.country,
+      temperature: response.main.temp,
+      description: response.weather[0].description
+
+    });
+  };
+  render(){
+    return (
+      <div className="App">
+        <Weather city={this.state.city}
+        country={this.state.country}
+        temperature={this.state.temperature}
+        description = {this.state.description}
+        />
+
+      </div>
+    );
+
+  }
+
 }
 
 export default App;
--- src/component/weather.component.jsx
+++ src/component/weather.component.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 
-const Weather = ()=>{
+const Weather = props=>{
 
   return(
-  <div>
-    <h1>
-      Weather App by H J & E
-    </h1>
+  <div className="container">
+    <div>
+      <h1>
+        {props.city}, {props.country}
+      </h1>
+    </div>
+    <div>
+      <h3>
+        The weather symbol
+      </h3>
+    </div>
+    <div>
+    <h4>
+      {props.temperature}
+    </h4>
+    <h5>
+      {props.description}
+    </h5>
+    </div>
   </div>
 
   );
